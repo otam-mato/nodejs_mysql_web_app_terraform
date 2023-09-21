5261badcb335a8a6dcd03bd292e8ad80b2a4a276
otam-mato <113034133+otam-mato@users.noreply.github.com>
2023-09-21 17:48:46 +0100

    Update main.tf

--- Terraform template/main.tf
+++ Terraform template/main.tf
@@ -1,28 +1,16 @@
-provider "aws" {
+rovider "aws" {
   region                    = "us-east-1"
   shared_config_files       = ["/home/ec2-user/.aws/config"]
   shared_credentials_files  = ["/home/ec2-user/.aws/credentials"]
 }
 
-
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
 data "aws_ssm_parameter" "current-ami" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
-
-
-# resource "aws_vpc" "vpc" {
-#   cidr_block = "10.0.0.0/16"
-#   #public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-#   enable_dns_hostnames = true
-#   enable_dns_support   = true
-
-#   tags = {
-#     Name = "test-vpc"
-#   }
-# }
 
 resource "aws_default_vpc" "default" {
   enable_dns_hostnames = true
@@ -32,31 +20,31 @@ resource "aws_default_vpc" "default" {
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-  vpc_id     = aws_default_vpc.default.id
-  # cidr_block = "10.0.1.0/24"
-  cidr_block = "172.31.98.128/25"
-  availability_zone = data.aws_availability_zones.available.names[0]
-
-  tags = {
-    Name = "test-public-subnet"
-  }
+data "aws_vpc" "default" {
+  default = true
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_default_vpc.default.id
-  # cidr_block = "10.0.2.0/24"
-  cidr_block = "172.31.99.128/25"
-  availability_zone = data.aws_availability_zones.available.names[1]
+locals {
+  subnet_count = 2  # Adjust this to the desired number of subnets
+  base_cidr_block = data.aws_vpc.default.cidr_block
+  subnet_bits = 8  # You can adjust the number of bits as needed
+}
+
+resource "aws_subnet" "subnets" {
+  count = local.subnet_count
+
+  vpc_id            = aws_default_vpc.default.id
+  cidr_block        = cidrsubnet(local.base_cidr_block, local.subnet_bits, count.index + 130)  # Offset by 8 to avoid conflicts
+  availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
-    Name     = "test-private-subnet"
+    Name = "test-subnet-${count.index + 1}"
   }
 }
 
 
 resource "aws_security_group" "ec2_security_group" {
-  name        = "test-ec2-security-group"
+  name        = "test-ec2-security-group1"
   description = "Allow ssh access"
   vpc_id      = aws_default_vpc.default.id
 
@@ -64,21 +52,21 @@ resource "aws_security_group" "ec2_security_group" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Add your SSH ingress rules here
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Add your HTTP ingress rules here
   }
   
   ingress {
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Add your custom application port ingress rules here
   }
 
   egress {
@@ -91,8 +79,8 @@ resource "aws_security_group" "ec2_security_group" {
 
 resource "aws_instance" "ec2_instance" {
   ami           = data.aws_ssm_parameter.current-ami.value
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
+  instance_type = "t2.medium"
+  subnet_id     = aws_subnet.subnets[0].id  # Use the first subnet created
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
   associate_public_ip_address = true
   key_name      = "test_delete"
@@ -116,7 +104,7 @@ npm install express
 }
 
 resource "aws_security_group" "rds_security_group" {
-  name        = "test-rds-security-group"
+  name        = "test-rds-security-group1"
   description = "Allow mysql access"
   vpc_id      = aws_default_vpc.default.id
 
@@ -124,13 +112,13 @@ resource "aws_security_group" "rds_security_group" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]  # Add your RDS ingress rules here
   }
 }
 
 resource "aws_db_subnet_group" "rds_subnet_group" {
-  name        = "test-rds-subnet-group"
-  subnet_ids  = [aws_subnet.private_subnet.id, aws_subnet.public_subnet.id]
+  name       = "test-rds-subnet-group1"
+  subnet_ids = aws_subnet.subnets[*].id
 }
 
 resource "aws_db_instance" "rds_instance" {
@@ -147,13 +135,13 @@ resource "aws_db_instance" "rds_instance" {
   publicly_accessible     = true
 }
 
+
 resource "null_resource" "setup_db" {
-  depends_on = [aws_db_instance.rds_instance] #wait for the db to be ready
+  depends_on = [aws_db_instance.rds_instance] # Wait for the DB to be ready
   provisioner "local-exec" {
     command = "mysql -h ${aws_db_instance.rds_instance.address} -u ${aws_db_instance.rds_instance.username} --password=${var.dbpassword}  ${var.dbname} < my_sql.sql"
   }
 }
-
 
 
 output "rds_hostname" {
@@ -183,4 +171,3 @@ output "ec2_public_dns" {
 output "ec2_public_ip" {
   value = aws_instance.ec2_instance.public_ip
 }
-
