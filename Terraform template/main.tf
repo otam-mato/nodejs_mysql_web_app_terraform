9a9d1acb3959353cedef2dbfc6bb29a2894f7181
otam-mato <113034133+otam-mato@users.noreply.github.com>
2023-09-23 14:07:48 +0100

    Update main.tf

--- Terraform template/main.tf
+++ Terraform template/main.tf
@@ -142,7 +142,7 @@ resource "aws_db_instance" "rds_instance" {
   vpc_security_group_ids  = [aws_security_group.rds_security_group.id]
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet_group.name
   skip_final_snapshot     = true
-  publicly_accessible     = false # Set to true to unrestrict public internet access for testing the connectivity
+  publicly_accessible     = true # Set to true to unrestrict public internet access for testing the connectivity
 }
 
 # Provisioning for RDS
