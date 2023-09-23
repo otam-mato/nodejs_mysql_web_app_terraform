c9f81ce6c4739e25b7b29579cd0118e3db0fd503
otam-mato <113034133+otam-mato@users.noreply.github.com>
2023-09-23 12:33:13 +0100

    Update main.tf

--- Terraform template/main.tf
+++ Terraform template/main.tf
@@ -113,7 +113,8 @@ resource "aws_security_group" "rds_security_group" {
     from_port   = 3306
     to_port     = 3306
     protocol    = "tcp"
-    cidr_blocks = [aws_security_group.ec2_security_group.id]  # I made the RDS intance accesible only from created EC2 instance.
+    security_groups  = [aws_security_group.ec2_security_group.id] # I made the RDS intance accesible only from created EC2 instance.
+    # cidr_blocks = ["0.0.0.0/0"]  use this attribute instead for testing the connectivity
   }
 }
 
